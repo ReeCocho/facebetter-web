6e8402f3eafa2d81960214fab52d2978857e70ce
Guraashish-Kainth <99357289+Guraashish-Kainth@users.noreply.github.com>
2022-11-04 18:33:05 -0400

    Merge branch 'tests'

---
+++ api.test.js
@@ -0,0 +1,124 @@
+const request = require("supertest");
+const { app, client, server } = require("./server");
+
+afterAll(async () => {
+    await client.close();
+    server.close();
+});
+
+describe("API Tests", () => {
+    test('login on the test user', async () => {
+        const data = {
+            Login: "test",
+            Password: "test"
+        };
+        const res = await request(app).post('/api/login').send(data).expect(200);
+        expect(res.body.Error).toBe(null); 
+    });
+
+    test('login on the test user with the incorrect password', async () => {
+        const data = {
+            Login: "test",
+            Password: "wrong_password_bozo"
+        };
+        const res = await request(app).post('/api/login').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null); 
+    });
+
+    test("login to a user that doesn't exist", async() => {
+        const data = {
+            Login: "$BAD_LOGIN$",
+            Password: "$BAD_PASSWORD$"
+        };
+        const res = await request(app).post('/api/login').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null);
+    });
+
+    test("registering a user that already exist", async() => {
+        const data = {
+            Login: "test",
+            Password: "totally_secure_password",
+            FirstName: "Test",
+            LastName: "User",
+            School: "",
+            Work: ""
+        };
+        const res = await request(app).post('/api/register').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null);
+    });
+
+    test("registering a user with bad credentials", async() => {
+        let data = {
+            Login: "",
+            Password: "password",
+            FirstName: "name",
+            LastName: "name",
+            School: "",
+            Work: ""
+        };
+        let res = await request(app).post('/api/register').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null);
+
+        data = {
+            Login: "login",
+            Password: "",
+            FirstName: "name",
+            LastName: "name",
+            School: "",
+            Work: ""
+        };
+        res = await request(app).post('/api/register').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null);
+
+        data = {
+            Login: "login",
+            Password: "password",
+            FirstName: "",
+            LastName: "name",
+            School: "",
+            Work: ""
+        };
+        res = await request(app).post('/api/register').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null);
+
+        data = {
+            Login: "login",
+            Password: "password",
+            FirstName: "name",
+            LastName: "",
+            School: "",
+            Work: ""
+        };
+        res = await request(app).post('/api/register').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null);
+
+        data = {
+            Login: "",
+            Password: "",
+            FirstName: "",
+            LastName: "",
+            School: "",
+            Work: ""
+        };
+        res = await request(app).post('/api/register').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null);
+    });
+
+    test('retrieving the profile of the test user', async () => {
+        const data = {
+            _id: "634b740db076952180cb8e5a",
+        };
+        const res = await request(app).post('/api/retrieveprofile').send(data).expect(200);
+        expect(res.body.Error).toBe(null);
+        expect(res.body.FirstName).toBe("Test");
+        expect(res.body.LastName).toBe("User");
+    });
+
+    test("retrieving the profile of a user that doesn't exist", async () => {
+        const data = {
+            _id: "bad_id",
+        };
+        const res = await request(app).post('/api/retrieveprofile').send(data).expect(200);
+        expect(res.body.Error).not.toBe(null);
+    });
+});
