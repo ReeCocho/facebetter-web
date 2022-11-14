ac54d40c29842404aef9a01cf464b7114503147c
SouloCider <luke.wittmayer@knights.ucf.edu>
2022-11-14 02:22:43 -0500

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './Profile.css';
+import axios from "axios";
 
 function HomeComp() {
 
+  var bp = require('./Path.js');
+  var searchName;
+  
+  function doSearch(){
+    // incoming: search (ex: {"search": "dennis"})
+    // outgoing: {Results: [ {_id: "6344e4ea7c568d2a25ed0f6f", FirstName: "Dennis", LastName: "Cepero", School: "UCF", Work: "Full Sail"}, {_id: "someoneelse", ...} ]}
+    axios
+    .post(bp.buildPath("api/searchprofiles") , {
+      search: searchName.value,
+    })
+    .then((res) => {
+      //console.log(res.data.Results);
+      localStorage.setItem("search_result", JSON.stringify(res.data.Results));
+      
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  var results = JSON.parse(localStorage.getItem("search_result"));
 
   return (
     <div className='main_div'>      
@@ -12,9 +34,17 @@ function HomeComp() {
 
       <h1>Get Started, Search For Another User!</h1>
       <input 
-          type="text"
-          id="searchName"
-        />
+        type="text"
+        id="searchName"
+        placeholder="Enter Name..."
+        ref={(c) => (searchName = c)}
+      />
+      <input 
+        type="submit"
+        id="searchBtn"
+        onClick={doSearch}
+      />
+
     </div>
   )
 }
