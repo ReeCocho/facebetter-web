a976b024397e2fead8fac4610b8e80fc16514364
Dennis Cepero <denniscepero@gmail.com>
2022-11-25 14:48:52 -0500

    Made picture on profile page circular

--- frontend/src/components/People_Search.js
+++ frontend/src/components/People_Search.js
@@ -38,7 +38,7 @@ function People({ first, last, login, picture}) {
 
   return (
     <div className='container'>
-        <img src={picture} alt="" id="search_picture"></img>
+        <img src={picture} alt="" className="search_picture"></img>
         <div>
           <h1>{first}&nbsp;{last}</h1>
         </div>
